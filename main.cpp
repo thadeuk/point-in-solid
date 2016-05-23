1f91322411d04c520fac60ff60441c490928fd71
Thadeu <thadeutucci@gmail.com>
2016-05-23 22:44:38 +0200

    fixed bounding box

--- main.cpp
+++ main.cpp
@@ -26,23 +26,29 @@ void print_xml(World &w)
   	cout << "<camera target=\"200,200,200\" directionSpherical=\"0,38," << 2000 << "\" angle=\"50\" far=\"" << 2000*2 << "\"/>" << endl;
   	cout << "<spotlight target=\"200,200,200\" directionSpherical=\"-30,50,2000\" angle=\"45\"/>" << endl;
   	cout << "<blockList color=\"128,128,128\" blocksize=\"10.0,10.0,10.0\">" << endl;
+    
+    int areaX = w.grid_max[0] - w.grid_min[0];
+    int areaY = w.grid_max[1] - w.grid_min[1];
+    int areaZ = w.grid_max[2] - w.grid_min[2];
 
-    for (int x = w.grid_min[0]; x <= w.grid_max[0]; x++) {
-        for (int y = w.grid_min[1]; y <= w.grid_max[1]; y++) {
-            for (int z = w.grid_min[2]; z <= w.grid_max[2]; z++) {
-                p1.z = z*M_SQRT2_2+0.5;
+    for (int z = 0; z <= areaZ; z++) {
+        for (int y = 0; y <= areaY; y++) {
+            for (int x = 0; x <= areaX; x++) {
+
+                p1.z = w.grid_min[2] + 0.5 + z*M_SQRT2_2;
                 if (z%2) {
-                    p1.x = x+1.0;
-                    p1.y = y+1.0;
+                    p1.x = w.grid_min[0]+x+1.0;
+                    p1.y = w.grid_min[1]+y+1.0;
                 }
                 else {
-                    p1.x = x+0.5;
-                    p1.y = y+0.5;
+                    p1.x = w.grid_min[0]+x+0.5;
+                    p1.y = w.grid_min[1]+y+0.5;
                 }
+                
                 while ((ret = w.point_in_polygon(p1, w.get_random_point() )) == 1) repeateds++;
                 if (ret == 2) {
                     point_numbers++;
-                    printf("<block position=\"%d,%d,%d\" color=\"0,0,255\"/>\n",x-w.grid_min[0], y-w.grid_min[1], z-w.grid_min[2]); 
+                    printf("<block position=\"%d,%d,%d\" color=\"0,0,255\"/>\n",x,y,z); 
                 }
             }
         }
@@ -56,7 +62,7 @@ main()
 {
     World w;
 
-    Obj_File obj("obj/voiture-lowresolution_fixed.obj");
+    Obj_File obj("obj/voiture-highresolution_fixed.obj");
     w.create_world();
 
     print_xml(w);
